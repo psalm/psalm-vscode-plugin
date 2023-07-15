2f4ecb042e4c961414e72b3728d0b5ad16a28cf1
Mark Li <Mark_Li@ReyRey.com>
2023-07-15 20:30:11 +0000

    ensure npm is installed

--- .vscode/runInTunnel.sh
+++ .vscode/runInTunnel.sh
@@ -3,11 +3,14 @@ if [ -z "$1" ];then
 else
     workspaceFolder=$1  
 fi
+echo Workspace: $workspaceFolder
 if [ ! -f "/tmp/code" ]; then #get vscode cli if it doesn't exist
     wget -O /tmp/code.tar.gz "https://code.visualstudio.com/sha/download?build=stable&os=cli-alpine-x64" && 
     tar -xf /tmp/code.tar.gz -C /tmp
 fi
+npm --prefix $workspaceFolder install
 outfile=$(npm --prefix $workspaceFolder run vsce:package | grep -o $workspaceFolder/psalm-vscode-plugin-*.vsix)
+echo Extension: $outfile
 /tmp/code --install-extension $outfile
 /tmp/code tunnel --name "psalm-vscode-plugin" --accept-server-license-terms
 #/tmp/code tunnel --name "psalm-vscode-plugin" --accept-server-license-terms --extensionDevelopmentPath=$workspaceFolder
