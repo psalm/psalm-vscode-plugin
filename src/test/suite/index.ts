1e3fd436f70087ca3401e675247631845337c71c
Mark Li <Mark_Li@ReyRey.com>
2023-07-19 01:37:22 -0500

    add tests

---
+++ src/test/suite/index.ts
@@ -0,0 +1,27 @@
+import * as path from 'path';
+import Mocha from 'mocha';
+import { glob } from 'glob';
+
+export async function run(): Promise<void> {
+    // Create the mocha test
+    const mocha = new Mocha({
+        ui: 'tdd',
+    });
+
+    const testsRoot = path.resolve(__dirname, '..');
+    const files = await glob('**/**.test.js', { cwd: testsRoot });
+    // Add files to the test suite
+    files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
+
+    try {
+        // Run the mocha test
+        mocha.run((failures) => {
+            if (failures > 0) {
+                throw new Error(`${failures} tests failed.`);
+            }
+        });
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
+}
